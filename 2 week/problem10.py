0da59b1f8efec74285992c80f3786952e09d2c48
Emil <emiliomaxmatrix@gmail.com>
2019-10-30 13:37:07 -0800

    week 2 problems

---
+++ 2 week/problem10.py
@@ -0,0 +1,6 @@
+import datetime
+import time
+import calendar
+tdy= datetime.date.today()
+ltime= time.localtime()
+print("Current date and time: ", tdy, ltime )
---
+++ 2 week/problem8.py
@@ -0,0 +1,8 @@
+country= "Armenia"
+years=int=10
+print(""" 'Hi, where are you from?'
+'I\'m from %s'
+'How long have you lived here?'
+'for %d years'
+ """
+%(country, years))
