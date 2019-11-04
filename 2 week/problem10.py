c2d16e28d5227f0b65618096dd711fdeeb1b0447
Emil <emiliomaxmatrix@gmail.com>
2019-11-04 14:35:55 -0800

    homework3

--- 2 week/problem10.py
+++ 2 week/problem10.py
@@ -1,6 +1,13 @@
 import datetime
 import time
 import calendar
-tdy= datetime.date.today()
-ltime= time.localtime()
-print("Current date and time: ", tdy, ltime )
+tdy= datetime.datetime.today()
+
+print("Current date and time: ", tdy)  
+print("current year :", tdy.year)
+print("current month :", tdy.month)
+print("current weekday :", tdy.weekday())
+print("current weekday :", tdy.isoweekday())
+tdelta= datetime.timedelta(days = 5)
+print("date today - 5 =", tdy - tdelta)
+print("date today + 5 =", tdy + tdelta)
