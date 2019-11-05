ab29760187c7c7952a04799cbd8e03231a4ff63f
Emil <emiliomaxmatrix@gmail.com>
2019-11-05 08:06:13 -0800

    home4

--- 2 week/home4.py
+++ 2 week/home4.py
@@ -5,7 +5,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("text", type= str)
 
 args = parser.parse_args()
-
+x= args.text.replace('usa', 'Armenia')
+x= x.replace('USA', 'Armenia')
 print("The given string :", args.text)
 print("The USA/usa count is :", args.text.lower().count('usa'))
-print("The new string: ", args.text.lower().replace('usa', 'Armenia'))
+print("The new string: ", x)
