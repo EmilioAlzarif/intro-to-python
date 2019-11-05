7208dd28e95335c51a4870138d96203fee110eb4
Emil <emiliomaxmatrix@gmail.com>
2019-11-05 14:54:01 -0800

    homework1 edited

--- week 2/Homework/hom1.py
+++ week 2/Homework/hom1.py
@@ -11,16 +11,25 @@ tdy= datetime.datetime.today()
 tdelta= datetime.timedelta(days= 0)
 print("Current date : ", tdy)
 
-if args.num_y:    
+if args.num_y and args.num_d:
     tdeltay= tdelta + datetime.timedelta(days= args.num_y * 365)
- 
-if args.num_d:
     tdeltad= tdelta + datetime.timedelta(days= args.num_d + 1)
-
     x = tdeltay + tdeltad
     print("Given days :", args.num_d)
     print("Given years :", args.num_y)
     print("Final date :", tdy + x)
+ 
+elif args.num_d:
+    tdeltad= tdelta + datetime.timedelta(days= args.num_d + 1)
+    print("Given days :", args.num_d)
+    print("Given years : none")
+    print("Final date :", tdy + tdeltad)
+
+elif args.num_y:    
+    tdeltay= tdelta + datetime.timedelta(days= args.num_y * 365)
+    print("Given days : none")
+    print("Given years :", args.num_y)
+    print("Final date :", tdy + tdeltay)
 
 else:
     print("Given days : None")
