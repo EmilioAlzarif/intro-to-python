006dc3b82b64a0adc5406c26ef9e3a8f7cf75fd3
Emil <emiliomaxmatrix@gmail.com>
2019-11-17 10:19:49 +0400

    practical 3 edited

--- week 4/Practical/pract3.py
+++ week 4/Practical/pract3.py
@@ -1,11 +1,10 @@
 name= "Batman"
-age = int = 15
-password = "sd&akm2131"
+age = int = 16
+password = "sd*akm2131"
 
 if name == "Batman":
     print("Welcome Mr. Batman!")
 if age < 16:
     print("Dear", name, "you are too young to register.")
-for x in '*' and '&':
-    if x not in password:
-        print("Please enter a different password.") 
+if "*" not in password and "&" not in password:
+    print("Please enter a different password.") 
